f84f1bdff84d3332d42c5a23f7100968f4490178
Andrew Schmelyun <schmelyun@gmail.com>
2020-07-31 02:44:32 -0400

    Adds routes, controllers, removes previous default ones

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'auth', 'prefix' => 'dashboard'], function() {
+
+    Route::get('/links', 'LinkController@index');
+    Route::get('/links/new', 'LinkController@create');
+    Route::post('/links/new', 'LinkController@store');
+    Route::get('/links/{link}', 'LinkController@edit');
+    Route::post('/links/{link}', 'LinkController@update');
+    Route::delete('/links/{link}', 'LinkController@destroy');
+
+    Route::get('/settings', 'UserController@edit');
+    Route::post('/settings', 'UserController@update');
+
+});
+
+Route::post('/visit/{link}', 'VisitController@store');
+
+Route::get('{user}', 'UserController@show');
