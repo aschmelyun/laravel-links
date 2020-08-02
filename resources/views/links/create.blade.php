cea89f0aaa74323be9bcca9b62b0aa5c4f824cd7
Andrew Schmelyun <schmelyun@gmail.com>
2020-08-01 23:45:23 -0400

    Adds link dashboard views

--- resources/views/links/create.blade.php
+++ resources/views/links/create.blade.php
@@ -5,6 +5,28 @@
             <div class="col-12 card">
                 <div class="card-body">
                     <h2 class="card-title">Create a new link</h2>
+                    <form action="/dashboard/links/new" method="post">
+                        <div class="row">
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="name">Link Name</label>
+                                    <input type="text" id="name" name="name" class="form-control" placeholder="My YouTube Channel">
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="name">URL</label>
+                                    <input type="text" id="url" name="url" class="form-control" placeholder="https://youtube.com/user/aschmelyun">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                @csrf
+                                <button type="submit" class="btn btn-primary">Save Link</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
