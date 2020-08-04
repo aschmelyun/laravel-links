280770a400cc1e51b1b69011faf821f1c15d20bc
Andrew Schmelyun <schmelyun@gmail.com>
2020-08-03 23:03:01 -0400

    Update implicit model binding column for User, change app name in .env.example

--- app/User.php
+++ app/User.php
@@ -38,4 +38,8 @@ class User extends Authenticatable
         return $this->hasManyThrough(Visit::class, Link::class);
     }
 
+    public function getRouteKeyName() {
+        return 'username';
+    }
+
 }
